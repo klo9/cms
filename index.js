1314437a724ed40b6bd03791527902c9ce5135cc
kl <kl@onesoftway.com>
2021-03-14 14:32:47 -0400

    update files

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+mongoose.connect('mongodb://localhost:27017/cms', {useUnifiedTopology: true, useNewUrlParser: true})
+    .then( res => {
+        console.log('mongodb connected successfully')
+    })
+    .catch( err => {
+        console.log('mongodb connection failed')
+    })
 
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine
+
 
 // routes
 app.use('/', (req, res) => {
