0aea01fdc6d703ac2e6bc0b290ffffe232785bda
kl <kl@onesoftway.com>
2021-03-14 14:24:05 -0400

    modified:   index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 
-app.listen(3000);
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// routes
+app.use('/', (req, res) => {
+    res.send('hello');
+})
+
+app.listen(8080);
